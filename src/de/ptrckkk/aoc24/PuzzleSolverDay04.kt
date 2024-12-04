e6d301e4f26e4d526015194d845df27f2c70feeb
ptrckkk <pmell@posteo.net>
2024-12-04 13:35:52 +0100

    Do preparations for Day 4

---
+++ src/de/ptrckkk/aoc24/PuzzleSolverDay04.kt
@@ -0,0 +1,25 @@
+package de.ptrckkk.aoc24
+
+class PuzzleSolverDay04 : PerDayPuzzleSolver() {
+
+    /**
+     * For the task description, see
+     * [https://adventofcode.com/2024/day/4](https://adventofcode.com/2024/day/4).
+     *
+     * @see [PerDayPuzzleSolver.solvePuzzleOne]
+     */
+    override fun solvePuzzleOne(pathToInputFile: String): Int {
+        TODO("Not yet implemented")
+    }
+
+    /**
+     * For the task description, see
+     * [https://adventofcode.com/2024/day/4#part2](https://adventofcode.com/2024/day/4#part2).
+     *
+     * @see [PerDayPuzzleSolver.solvePuzzleTwo]
+     */
+    override fun solvePuzzleTwo(pathToInputFile: String): Int {
+        TODO("Not yet implemented")
+    }
+
+}
