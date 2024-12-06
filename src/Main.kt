19e7358e9e4746ecf128f7e24ac22eda56c68fbb
ptrckkk <pmell@posteo.net>
2024-12-06 11:44:59 +0100

    Do preparations for Day 6

--- src/Main.kt
+++ src/Main.kt
@@ -11,6 +11,24 @@ fun assertResult(expected: Int, actual: Int, message: String) {
 }
 
 fun main() {
+    // Day 6, example Puzzle 1
+    val solverDay06 = PuzzleSolverDay06()
+    val actualSampleResultDay6Puzzle1 = solverDay06.solvePuzzleOne("example-input-day-06.txt")
+    assertResult(0, actualSampleResultDay6Puzzle1, "Solution for 'Example Day 6, Puzzle 1' wrong")
+
+    // Day 6, Puzzle 1
+    val actualResultDay6Puzzle1 = solverDay06.solvePuzzleOne("input-day-06.txt")
+    print(actualResultDay6Puzzle1)
+    assertResult(0, actualResultDay6Puzzle1, "Solution for 'Day 6, Puzzle 1' wrong")
+
+    // Day 6, example Puzzle 2
+    val actualSampleResultDay6Puzzle2 = solverDay06.solvePuzzleTwo("example-input-day-06-puzzle-2.txt")
+    assertResult(0, actualSampleResultDay6Puzzle2, "Solution for 'Example Day 6, Puzzle 2' wrong")
+
+    // Day 6, Puzzle 2
+     val actualResultDay6Puzzle2 = solverDay06.solvePuzzleTwo("input-day-06.txt")
+     assertResult(0, actualResultDay6Puzzle2, "Solution for 'Day 6, Puzzle 2' wrong")
+
     // Day 5, example Puzzle 1
     val solverDay05 = PuzzleSolverDay05()
     val actualSampleResultDay5Puzzle1 = solverDay05.solvePuzzleOne("example-input-day-05.txt")
